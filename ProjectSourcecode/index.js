3c1f82ca9cb6de3fa532b527a312df953f9e0eca
Ben Getches <benjamin.getches@colorado.edu>
2024-12-09 13:13:37 -0700

    added presentation

--- ProjectSourcecode/index.js
+++ ProjectSourcecode/index.js
@@ -98,7 +98,7 @@ const transporter = nodemailer.createTransport({
 
 // sql config
 const dbConfig = {
-  host: 'dpg-csvpgvilqhvc73bgrnu0-a', // the database server process.env.DB_HOST,//dpg-csvpgvilqhvc73bgrnu0-a
+  host: 'db', // the database server process.env.DB_HOST,//dpg-csvpgvilqhvc73bgrnu0-a
   port: 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
